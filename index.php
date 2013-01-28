1f045c837370535844b0391ff205ae6eb19c0cfa
Shahzeb <shahzeb001@gmail.com>
2013-01-27 22:51:25 -0500

    Added "Fork Me" banner

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $button[1] = '<a href="?page='.$b.'">></a>';
 ?>
 	<body>
 		<div id="header">
-			<a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>
+			<a href="https://github.com/shahzeb1/full-youtube-movies"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>
 			<h1>YouTube Movies</h1>
 			<h3>[<a href="#" onClick="listings();">Show all listings</a> | <a href="#" onClick="lights();">Hit the lights</a>]</h3>
 			<h2><?=$title;?></h2>
